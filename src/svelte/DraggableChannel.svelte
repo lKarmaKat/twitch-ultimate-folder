d233766396577dd616879b499108956ec989441b
KarmaKat <lloyd@gmail.com>
2026-01-07 18:55:17 +0100

    Removed magnifier after pseudo element on tooltip

--- src/svelte/DraggableChannel.svelte
+++ src/svelte/DraggableChannel.svelte
@@ -230,13 +230,7 @@ function tooltip(node, params) {
 		white-space: nowrap;
 		position: relative;
 		padding-top: 0.35rem;
-		cursor: zoom-in;
 		border-bottom: 1px solid currentColor;
-	}
-	
-	:global(.tooltip::after) {
-		margin: 0 0.15rem 0 0.25rem;
-		content: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="-50 -50 100 100"%3E%3Cg fill="none" stroke="hsl(0, 0%25, 30%25)" stroke-linecap="round"%3E%3Cpath stroke-width="8" d="M -13 -13 m 0 -10 v 20 m 10 -10 h -20" /%3E%3Cg stroke-width="14"%3E%3Ccircle r="30" cx="-13" cy="-13" /%3E%3Cpath d="M 24 24 l 18 18" /%3E%3C/g%3E%3C/g%3E%3C/svg%3E');
 	}
 	
 	:global(#tooltip) {
