42d14e75a1f94a9037e366796101f8e75ff63ab0
KarmaKat <lloyd@gmail.com>
2026-05-04 17:18:56 +0200

    cleanup configManager

--- src/content_script/configManager.ts
+++ src/content_script/configManager.ts
@@ -8,30 +8,22 @@ class ConfigManager {
     extensionId: string = "ijodiaomnnnjljemidchdifmpnnmcnlg";
     initComplete: boolean = false;
     channelsPickRef = writable<StreamsInfos[]>([]);
-    // streamInfoPromise: Promise<any>;
-    // currentConfigPromise;
-    // channelsRef = writable([]);
+    bridge: PortConnector;
     save = [];
 	channelsConfigList = writable<UserConfigs>();
     currentConfigName = writable<string>('');
     selectedConfig = writable<I_CONFIG>();
     autre = derived([this.channelsConfigList, this.currentConfigName], ([$channelsConfigList, $currentConfigName]) => {
-    // console.log("update");
     if ($channelsConfigList?.configsList) {
         let index = $channelsConfigList.configsList.find(conf => conf.rootList.name === $currentConfigName);
         if (index) {
             this.selectedConfig.set(index);
-          // console.log("selected config", $selectedConfig, index)
           return index;
         }
         throw new Error('ConfigName not found in configList');
     }
   });
-    bridge: PortConnector;
-    // display = chaines;
-    f : boolean = true;
     constructor() {
-        // console.log("Constructeur config manager")
         this.startPort();
         this.autre.subscribe(e => e);
     }
@@ -52,88 +44,22 @@ class ConfigManager {
                     else return alphaSort(a,b);
                 };
             if (msg.type === CST.GET_CURRENT_CONFIGURATION) {
-                // TODO voir si sauvegarder une liste ne vas pas écraser des données en cours de modif dans une autre popup.
                 this.currentConfigName.set(msg.data.currentConfig);
-                // console.log("Setting config in config manager", msg);
                 if (msg.data) {
                     this.channelsConfigList.set(structuredClone(msg.data));
                 }
             } else if (msg.type === CST.GET_STREAM_INFO) {
-                // for(let i = 0; i < msg.data.length; i++)
-                //     console.log("GET_STREAM_INFO", msg.data[i].channel_name, msg.data[i].viewer_count)
-
                 let all = [...msg.data, CST.ALL_OTHER_CHANNELS_ELEMENT]
                 all.sort(alphaSortCallback)
                 
                 this.channelsPickRef.set(all);
-                // console.log("updating", msg.data);
-                // this.save = streamInfo;
             } else if (msg.type === CST.UPDATE_STREAM_INFO) {
-                // console.log("UPDATING")
-                // msg.data.sort((a: StreamsInfos, b: StreamsInfos) => {
-                //     let alphaSort = (a: StreamsInfos, b: StreamsInfos) => {
-                //         return a.channel_name.localeCompare(b.channel_name);
-                //     }
-                //     if (a.isLive && b.isLive) return alphaSort(a,b);
-                //     else if (a.isLive) return -1;
-                //     else if (b.isLive) return 1;
-                //     else return alphaSort(a,b);
-                // })
-                // );
-                // msg.data.forEach((d: StreamsInfos) => {
-                //     if (d.channel_name.toLowerCase().includes("alphacast")) {
-                //         if (this.f) {
-                //             d.isLive = false;
-                //             this.f = !this.f
-                //         } else {
-                //             this.f = !this.f
-                //             d.isLive = true;
-                //         }
-                //         console.log(d);
-                //     }
-                //     // if (Math.random() > 0.5) {
-                //     //     d.viewer_count = d.viewer_count + 5;
-                //     // } else {
-                //     //     d.viewer_count = d.viewer_count - 5;
-                //     // }
-                // })
-                // let m = new Map();
-                // for (const onlineChannel of msg.data.filter((i: StreamsInfos) => i.isLive)) {
-                //     m.set(onlineChannel.channel_id, onlineChannel);
-                // }
-                // this.channelsPickRef.update(liste => {
-                //     for (let chaine in liste) {
-                //         if (liste[chaine]) {
-                //             // liste[chaine].viewer_count = 666;
-                //             let currentRef = m.get(liste[chaine].channel_id);
-                //             if (currentRef) {
-                //                 liste[chaine].isLive = true;
-                //                 liste[chaine].viewer_count = currentRef.viewer_count;
-                //                 liste[chaine].title = currentRef.title;
-                //                 liste[chaine].game_name = currentRef.game_name;
-                //             } else {
-                //                 liste[chaine].isLive = false;
-                //                 liste[chaine].viewer_count = 0;
-                //             }
-                //         }
-                //     }
-                //     return liste;
-                // })
-                // console.log(" updating updating");
-
-                // for(let i = 0; i < msg.data.length; i++)
-                //     console.log("UPDATE_STREAM_INFO", msg.data[i].channel_name, msg.data[i].viewer_count)
-
-                // this.channelsPickRef.update(e => {
-                //     return e;
-                // })
                 let all = [...msg.data, CST.ALL_OTHER_CHANNELS_ELEMENT]
                 all.sort(alphaSortCallback)
                 this.channelsPickRef.set(all);
                 this.channelsConfigList.update(liste => liste);
             }
         }
-        // console.log("ConfigManager starting port")
         this.bridge = new PortConnector(dataReceivedCallback);
     }
 
