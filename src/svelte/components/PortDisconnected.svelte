d26449dcb5d0cfa6eb58e1e8c1766fdf063bc81b
KarmaKat <lloyd@gmail.com>
2026-06-20 17:56:30 +0200

    update portDisconnected to something a little bit graceful

--- src/svelte/components/PortDisconnected.svelte
+++ src/svelte/components/PortDisconnected.svelte
@@ -2,39 +2,30 @@
     import { _ } from 'svelte-i18n';
 </script>
 
+
 <style>
-    .container {
+/* HTML: <div class="loader"></div> */
+    .loader {
+    width: 100%;
+    height: 0.5rem;
+    background: 
+        linear-gradient(90deg,rgba(0, 0, 0, 0.171) 33%,#0005 50%,rgba(0, 0, 0, 0.308) 66%)
+        #bf94ff;
+    background-size:300% 100%;
+    animation: l1 1s infinite linear;
+    }
+    @keyframes l1 {
+    0% {background-position: right}
+    }
+    .flex-container {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
     }
-
-    :global(.dark) p {
-        color: white;
-    }
-    :global(.light) p {
-        color: black;
-    }
-    
-    :global(.dark) svg {
-        fill: #bf94ff; 
-        stroke: #bf94ff; 
-    }
-    :global(.light) svg {
-        fill: #7b3fc9;
-        stroke: #7b3fc9;
-    }
-
-
 </style>
 
-
-
-
-
-
-<div class="container">
-    <svg width="64px" height="64px" viewBox="-1.7 0 20.4 20.4" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M16.406 10.283a7.917 7.917 0 1 1-7.917-7.917 7.916 7.916 0 0 1 7.917 7.917zM9.48 14.367a1.003 1.003 0 1 0-1.004 1.003 1.003 1.003 0 0 0 1.004-1.003zM7.697 11.53a.792.792 0 0 0 1.583 0V5.262a.792.792 0 0 0-1.583 0z"></path></g></svg>
-    <p>{$_('status.portDisconnected')}</p>
+<div class="flex-container">
+    <p data-testid="status">{$_('status.portDisconnected')}</p>
+    <div class="loader"></div>
 </div>
